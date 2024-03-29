ab535647bb6c71c1638fc4c0a507149acfa9e030
Miguel Bueno <bueno.reyes.miguel@gmail.com>
2024-03-29 18:38:52 +0100

    Modified requirements.txt

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from predict import Item, predict
+import uvicorn
 
 app = FastAPI()
 
@@ -23,3 +24,6 @@ async def predict_price(item: Item):
     prediction = predict(item)
     status_code = 200 if prediction else 404  # Set status code based on prediction availability
     return {"prediction": prediction, "status_code": status_code}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
